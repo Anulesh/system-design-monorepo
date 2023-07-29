54a9b7a110f78fac7865f816f610eeaa609b54ab
Anulesh Tiwari <anulesh.tiwari@phonepe.com>
2023-07-29 16:01:25 +0530

    work in progress

--- infinite-scroll-vanilla/src/virtual-list/virtual-list.ts
+++ infinite-scroll-vanilla/src/virtual-list/virtual-list.ts
@@ -19,20 +19,27 @@ const enum ScrollDirection {
 export class VirtualListComponent<T> extends Component<Props<T>, State> {
   TOP_OBSERVER_ELEMENT: HTMLElement = null;
   BOTTOM_OBSERVER_ELEMENT: HTMLElement = null;
+  ELEMENTS_LIMIT = this.props.pageSize * 2;
+  ELEMENTS_POOL = []
 
   init(): void {
     const [topObserver] = intersectionObserver(this.root, async ([entry]) => {
       if (entry.intersectionRatio > 0.1) {
         await this.update(ScrollDirection.UP);
       }
-    });
+    },
+    undefined,
+    {className: "top-observer"}
+    );
     const [bottomObserver] = intersectionObserver(
       this.root,
       async ([entry]) => {
         if (entry.intersectionRatio > 0.1) {
           await this.update(ScrollDirection.DOWN);
         }
-      }
+      },
+      undefined,
+      {className: "bottom-observer"}
     );
     this.TOP_OBSERVER_ELEMENT = topObserver;
     this.BOTTOM_OBSERVER_ELEMENT = bottomObserver;
@@ -50,7 +57,21 @@ export class VirtualListComponent<T> extends Component<Props<T>, State> {
     }
   }
   #handleTopIntersection = async () => {};
-  #handleBottomIntersection = async () => {};
+  #handleBottomIntersection = async () => {
+    const {pageSize} = this.props;
+    // number of elements rendered
+    const count = this.state.start - this.state.end;
+    const data = await this.props.load(this.state.end, pageSize)
+    // handling the case when elements limit has not reached yet
+    if(count<this.ELEMENTS_LIMIT) {
+        this.#initElementsPool(data)
+        this.state.end+=this.props.pageSize
+    }
+    else if (count==this.ELEMENTS_LIMIT) {
+
+        // logic
+    }
+  };
 
   getComponentId(): string {
     return "feed";
